f298605c8eeed8caab92e2a31de311019bf5ceb7
dkrimmer84 <dominic.krimmer@gmail.com>
2020-01-27 14:49:04 -0500

    changed mandatory field

--- models/res_partner.py
+++ models/res_partner.py
@@ -7,6 +7,6 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
     _name = 'res.partner'
 
-    tribute_id = fields.Many2one('dian.tributes', string="Tributos", required = True)
-    fiscal_responsability_ids = fields.Many2many('dian.fiscal.responsability', string="Responsabilidad fiscal", required = True)
+    tribute_id = fields.Many2one('dian.tributes', string="Tributos", required = False)
+    fiscal_responsability_ids = fields.Many2many('dian.fiscal.responsability', string="Responsabilidad fiscal", required = False)
     is_foreign = fields.Char('Is foreign')
