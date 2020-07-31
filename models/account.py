190c09b26b6bf89beca8e6ca8742c720e8818482
john <sebastian80_23@hotmail.com>
2020-07-31 16:29:47 -0500

    Nuevos cambios Dian

--- models/account.py
+++ models/account.py
@@ -16,3 +16,83 @@ class AccountTax(models.Model):
                             ('ZZ','Nombre de la figura tributaria')
                     ],string="Tributo DIAN")
 
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+
+    debit_note_sequence_id = fields.Many2one('ir.sequence', string='Debit Note Entry Sequence',
+        help="This field contains the information related to the numbering of the dedit note entries of this journal.", copy=False)
+
+    debit_note_sequence_number_next = fields.Integer(string='Nota de débito: Número siguiente',
+        help='The next sequence number will be used for the next dedit note.',
+        compute='_compute_debit_note_seq_number_next',
+        inverse='_inverse_debit_note_seq_number_next')
+
+    debit_note_sequence = fields.Boolean(string='Dedicated Dedit Note Sequence', help="Check this box if you don't want to share the same sequence for invoices and dedit notes made from this journal", default=False)
+
+
+    @api.multi
+    # do not depend on 'debit_note_sequence_id.date_range_ids', because
+    # debit_note_sequence_id._get_current_sequence() may invalidate it!
+    @api.depends('debit_note_sequence_id.use_date_range', 'debit_note_sequence_id.number_next_actual')
+    def _compute_debit_note_seq_number_next(self):
+        '''Compute 'sequence_number_next' according to the current sequence in use,
+        an ir.sequence or an ir.sequence.date_range.
+        '''
+        for journal in self:
+            if journal.debit_note_sequence_id and journal.debit_note_sequence:
+                sequence = journal.debit_note_sequence_id._get_current_sequence()
+                journal.debit_note_sequence_number_next = sequence.number_next_actual
+            else:
+                journal.debit_note_sequence_number_next = 1
+
+    @api.multi
+    def _inverse_debit_note_seq_number_next(self):
+        '''Inverse 'debit_note_sequence_number_next' to edit the current sequence next number.
+        '''
+        for journal in self:
+            if journal.debit_note_sequence_id and journal.debit_note_sequence and journal.debit_note_sequence_number_next:
+                sequence = journal.debit_note_sequence_id._get_current_sequence()
+                sequence.number_next = journal.debit_note_sequence_number_next
+
+    @api.model
+    def create(self, vals):
+        if vals.get('type') in ('sale', 'purchase') and vals.get('debit_note_sequence') and not vals.get('debit_note_sequence_id'):
+            vals.update({'debit_note_sequence_id': self.sudo()._create_sequence_debit_note(vals, debitnote=True).id})
+        journal = super(AccountJournal, self).create(vals)
+        return journal
+
+    @api.multi
+    def write(self, vals):
+        # create the relevant debit note sequence
+        if vals.get('debit_note_sequence'):
+            for journal in self.filtered(lambda j: j.type in ('sale', 'purchase') and not j.debit_note_sequence_id):
+                journal_vals = {
+                    'name': journal.name,
+                    'company_id': journal.company_id.id,
+                    'code': journal.code,
+                    'debit_note_sequence_number_next': vals.get('debit_note_sequence_number_next', journal.debit_note_sequence_number_next),
+                }
+                journal.debit_note_sequence_id = self.sudo()._create_sequence_debit_note(journal_vals, debitnote=True).id
+        result = super(AccountJournal, self).write(vals)
+        return result
+
+    @api.model
+    def _create_sequence_debit_note(self, vals, debitnote=False):
+        """ Create new no_gap entry sequence for every new Journal"""
+        prefix = self._get_sequence_prefix(vals['code'], debitnote)
+        seq_name = debitnote and vals['code'] + _(': Debit note') or vals['code']
+        seq = {
+            'name': _('%s Sequence') % seq_name,
+            'implementation': 'no_gap',
+            'prefix': prefix,
+            'padding': 4,
+            'number_increment': 1,
+            'use_date_range': True,
+        }
+        if 'company_id' in vals:
+            seq['company_id'] = vals['company_id']
+        seq = self.env['ir.sequence'].create(seq)
+        seq_date_range = seq._get_current_sequence()
+        seq_date_range.number_next = debitnote and vals.get('debit_note_sequence_number_next', 1) or vals.get('sequence_number_next', 1)
+        return seq
--- models/res_partner.py
+++ models/res_partner.py
@@ -10,3 +10,113 @@ class ResPartner(models.Model):
     tribute_id = fields.Many2one('dian.tributes', string="Tributos", required = False)
     fiscal_responsability_ids = fields.Many2many('dian.fiscal.responsability', string="Responsabilidad fiscal", required = False)
     is_foreign = fields.Char('Is foreign')    
+
+
+    @api.onchange('x_name1', 'x_name2', 'x_lastname1', 'x_lastname2', 'companyName',
+                  'pos_name', 'companyBrandName')
+    def _concat_name(self):
+        """
+        This function concatenates the four name fields in order to be able to
+        search for the entire name. On the other hand the original name field
+        should not be editable anymore as the new name fields should fill it up
+        automatically.
+        @return: void
+        """
+        # Avoiding that "False" will be written into the name field
+        if self.x_name1 is False:
+            self.x_name1 = ''
+
+        if self.x_name2 is False:
+            self.x_name2 = ''
+
+        if self.x_lastname1 is False:
+            self.x_lastname1 = ''
+
+        if self.x_lastname2 is False:
+            self.x_lastname2 = ''
+
+        # Collecting all names in a field that will be concatenated
+        nameList = [
+            self.x_name1,
+            self.x_name2,
+            self.x_lastname1,
+            self.x_lastname2
+        ]
+
+        formatedList = []
+        if self.companyName is False:
+            if self.type == 'delivery':
+                self.name = self.pos_name
+                self.x_name1 = False
+                self.x_name2 = False
+                self.x_lastname1 = False
+                self.x_lastname2 = False
+                self.doctype = 1
+            else:
+                for item in nameList:
+                    if item is not '':
+                        formatedList.append(item)
+                self.name = ' ' .join(formatedList)
+        else:
+            # Some Companies are know for their Brand, which could conflict from the users point of view while
+            # searching the company (e.j. o2 = brand, Telefonica = Company)
+            if self.companyBrandName is not False:
+                delimiter = ', '
+                company_list = (self.companyBrandName, self.companyName)
+                self.name = delimiter.join(company_list)
+            else:
+                self.name = self.companyName    
+
+
+    @api.onchange('x_name1')
+    def onChangeNameUpper(self):
+        """
+        Permite que cuando se termine de escribir se pueda pasar el campo
+        x_name1 automaticamente a mayuscula
+        @return: void
+        """
+        if self.x_name1 is not False:
+            self.x_name1 = self.x_name1
+
+    @api.onchange('x_name2')
+    def onChangeName2Upper(self):
+        """
+        Permite que cuando se termine de escribir se pueda pasar el campo
+        x_name2 automaticamente a mayuscula
+        @return: void
+        """
+        if self.x_name2 is not False:
+            self.x_name2 = self.x_name2             
+
+    @api.onchange('x_lastname1')
+    def onChangeLastNameUpper(self):
+        """
+        Permite que cuando se termine de escribir se pueda pasar el campo
+        x_lastname1 automaticamente a mayuscula
+        @return: void
+        """
+        if self.x_lastname1 is not False:
+            self.x_lastname1 = self.x_lastname1
+
+
+    @api.onchange('x_lastname2')
+    def onChangeLastName2Upper(self):
+        """
+        Permite que cuando se termine de escribir se pueda pasar el campo
+        x_lastname2 automaticamente a mayuscula
+        @return: void
+        """
+        if self.x_lastname2 is not False:
+            self.x_lastname2 = self.x_lastname2
+
+
+                        
+    @api.onchange('companyName')
+    def onChangeCompanyNUpper(self):
+        """
+        Permite que cuando se termine de escribir se pueda pasar el campo
+        companyName automaticamente a mayuscula
+        @return: void
+        """
+        if self.companyName is not False:
+            self.companyName = self.companyName
