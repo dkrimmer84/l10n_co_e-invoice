667df0c353a1bdff711f143cf306129b6e7625c9
sebas <sebastian80_23@hotmail.com>
2021-08-10 09:22:09 -0500

    Update models.py
    Adding new changes for dian 1.8

--- models/models.py
+++ models/models.py
@@ -3,7 +3,15 @@ from openerp import api, fields, models, _
 from openerp.exceptions import UserError, ValidationError
 from datetime import datetime, timedelta, date
 
+class ProductBrand(models.Model):
+    _name = "product.brand"
 
+    name = fields.Char('Marca', required = True)
+
+class ProductModel(models.Model):
+    _name = "product.model"
+
+    name = fields.Char('Model', required = True)
 
 class IrAttachment(models.Model):
     _inherit = "ir.attachment"
@@ -15,12 +23,13 @@ class IrAttachment(models.Model):
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    operation_type = fields.Selection([('01','Combustible'),('02','Emisor es Autoretenedor'),('03','Excluidos y Exentos'),
-    ('04','Exportación'),('05','Generica'),('06','Generica con pago anticipado'),
-    ('07','Generica con periodo de facturacion'),('08','Consorcio'),('09','Servicios AIU'),('10','Estandar'),
-    ('11','Mandatos bienes'),('12','Mandatos Servicios'),('00', 'No Aplica')], string='Tipo de operación DIAN')
+    operation_type = fields.Selection([('09','Servicios AIU'),('10','Estandar'),
+    ('11','Mandatos bienes')], string='Tipo de operación DIAN')
 	
     product_UNSPSC_id = fields.Many2one('dian.unspsc.product', string="Producto UNSPSC")
     segment_name = fields.Char(string="Segmento UNSPSC", readonly=True, related='product_UNSPSC_id.segment_id.name')
     family_name = fields.Char(string="Familia UNSPSC", readonly=True, related='product_UNSPSC_id.family_id.name')
     class_name = fields.Char(string="Segmento UNSPSC", readonly=True, related='product_UNSPSC_id.class_id.name')
+
+    brand_id = fields.Many2one('product.brand', 'Marca')
+    model_id = fields.Many2one('product.model', 'Modelo')
